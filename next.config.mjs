fb44cb73be6ddc2ec198590e8df356732db5e337
Lucas Laibly <lucaslaibly@Lucass-MacBook-Pro.local>
2025-09-22 08:47:12 -0400

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 }
 
 export default nextConfig
