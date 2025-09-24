10f1d8cce6f02326283af059df0d0f2713b28ac6
Lucas Laibly <lucaslaibly@Lucass-MacBook-Pro.local>
2025-09-23 21:56:53 -0400

    remove export from config, update themeprovider children

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,10 +1,15 @@
 'use client'
 
 import * as React from 'react'
+
 import {
   ThemeProvider as NextThemesProvider,
   type ThemeProviderProps,
 } from 'next-themes'
+
+type Props = ThemeProviderProps & {
+  children: React.ReactNode
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
